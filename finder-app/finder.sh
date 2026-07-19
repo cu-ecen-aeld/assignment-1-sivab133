e244b4e40b5388c15f0413d8c045acbb65752b52
sivab133 <sivab133@Ubuntu.myguest.virtualbox.org>
2026-07-19 18:00:43 +0530

    Completed Assignment 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+	echo "Error : Two Arguments Required"
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d "$filesdir" ]
+then
+	echo "Error: Directory is not exits"
+	exit 1
+fi
+
+filecount=$(find "$filesdir" -type f | wc -l)
+matchcount=$(grep -r "$searchstr" "$filesdir" 2>/dev/null |wc -l)
+
+echo "The number of files are $filecount and the number of matching lines are $matchcount"
+
+exit 0
---
+++ finder-app/writer.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+echo "Error : Two argument required"
+exit 1
+fi
+
+writefile=$1
+writestr=$2
+
+mkdir -p "$(dirname "$writefile")"
+
+echo "$writestr" > "$writefile"
+
+if [ $? -ne 0 ]
+then
+echo "Error creating file"
+exit 1
+fi
+
+exit 0
+
